5cf229febab2a1f8adc597266f3708d7ff6ce756
Sebastien Delrue <seb.delrue@gmail.com>
2023-10-16 19:52:29 -0400

    Cleaning + type for uri api

--- src/api/uri.ts
+++ src/api/uri.ts
@@ -9,74 +9,49 @@ export async function getNodeByURI(uri: string) {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       query: `query GetNodeByURI($uri: String!) {
-                        nodeByUri(uri: $uri) {
-                            __typename
-                            isContentNode
-                        isTermNode
-                        ... on Post {
-                        id
-                        title
-                        date
-                        uri
-                        excerpt
-                        content
-                        categories {
-                            nodes {
-                            name
-                            uri
-                            }
-                        }
-                        featuredImage {
-                            node {
-                            srcSet
-                            sourceUrl
-                            altText
-                            mediaDetails {
-                                height
-                                width
-                            }
-                            }
-                        }
-                        }
-                        ... on Page {
-                        id
-                        title
-                        uri
-                        date
-                        content
-                        }
-                        ... on Category {
-                        id
-                        name
-                        posts {
-                            nodes {
-                            date
-                            title
-                            excerpt
-                            uri
-                            categories {
-                                nodes {
-                                name
-                                uri
-                                }
-                            }
-                            featuredImage {
-                                node {
-                                srcSet
-                                sourceUrl
-                                altText
-                                mediaDetails {
-                                    height
-                                    width
-                                }
-                                }
-                            }
-                            }
-                        }
-                        }
-                    }
-                    }
-                `,
+          nodeByUri(uri: $uri) {
+            __typename
+            isContentNode
+            isTermNode
+            ... on Post {
+              id
+              title
+              date
+              uri
+              excerpt
+              content
+              categories {
+                nodes {
+                  name
+                  uri
+                }
+              }
+              featuredImage {
+                node {
+                  srcSet
+                  sourceUrl
+                  altText
+                  mediaDetails {
+                    height
+                    width
+                  }
+                }
+              }
+            }
+            ... on Page {
+              id
+              title
+              uri
+              date
+              content
+            }
+            ... on Category {
+              id
+              name
+            }
+          }
+        }
+      `,
       variables: {
         uri: newUri
       }
@@ -84,4 +59,31 @@ export async function getNodeByURI(uri: string) {
   })
   const { data } = await response.json()
   return data
+}
+
+export type Post = {
+  __typename: 'Post'
+  id: string
+  title: string
+  date: string
+  uri: string
+  excerpt: string
+  content: string
+  categories: {
+    nodes: {
+      name: string
+      uri: string
+    }[]
+  }
+  featuredImage: {
+    node: {
+      srcSet: string
+      sourceUrl: string
+      altText: string
+      mediaDetails: {
+        height: number
+        width: number
+      }
+    }
+  }
 }
