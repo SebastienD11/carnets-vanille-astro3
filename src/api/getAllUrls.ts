5b78cb6058106bea2cd198adb39bb4401fb233ee
Sebastien Delrue <seb.delrue@gmail.com>
2023-10-19 20:24:22 -0400

    Update getAllUrls.ts

--- src/api/getAllUrls.ts
+++ src/api/getAllUrls.ts
@@ -1,10 +1,18 @@
-export async function getAllUris(lang: string = 'fr') {
-  const response = await fetch(
-    import.meta.env.WORDPRESS_REST_API_URL + `/posts?per_page=100&_fields=slug&lang=${lang}`
-  )
-  const data = await response.json()
+type Node = {
+  slug: string
+}
 
-  const uris = data.map((node) => {
+// Todo:
+// - While loop to get all post (REST API limit to 100 posts per query)
+
+export async function getAllUris(lang: string = 'fr') {
+  const posts = await getAllPostsUrils(lang)
+  const tags = await getAllTagsUrils(lang)
+
+  let data: Node[] = posts
+  data = data.concat(tags)
+
+  const uris = data.map((node: Node) => {
     return {
       params: {
         uri: node.slug
@@ -12,8 +20,26 @@ export async function getAllUris(lang: string = 'fr') {
     }
   })
 
-  console.log(lang + ` uris posts`)
-  console.log(uris)
-
   return uris
+}
+
+const getAllPostsUrils = async (lang: string) => {
+  const res = await fetch(
+    import.meta.env.WORDPRESS_REST_API_URL + `/posts?per_page=100&_fields=slug&lang=${lang}`
+  )
+  const posts: Node[] = await res.json()
+  return posts
+}
+
+const getAllTagsUrils = async (lang: string) => {
+  const res = await fetch(
+    import.meta.env.WORDPRESS_REST_API_URL + `/tags?per_page=100&_fields=slug&lang=${lang}`
+  )
+  const tags: Node[] = await res.json()
+
+  tags.map((tag: Node) => {
+    tag.slug = 'tag/' + tag.slug
+  })
+
+  return tags
 }
