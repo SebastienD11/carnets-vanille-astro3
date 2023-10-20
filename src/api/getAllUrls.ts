4e6da3bfb56a411be57b951e25e8d1f32942b96c
Sebastien Delrue <seb.delrue@gmail.com>
2023-10-19 20:09:05 -0400

    Use Rest API for getAllUris

--- src/api/getAllUrls.ts
+++ src/api/getAllUrls.ts
@@ -1,46 +1,19 @@
 export async function getAllUris(lang: string = 'fr') {
-  const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: `query GetAllUris($lang: String!) {
-          posts(first: 150, where: {language: $lang}) {
-            nodes {
-              uri
-            }
-          }
-          tags(first: 150, where: {language: $lang}) {
-            nodes {
-              uri
-            }
-          }
-        }`,
-      variables: {
-        lang: lang
+  const response = await fetch(
+    import.meta.env.WORDPRESS_REST_API_URL + `/posts?per_page=100&_fields=slug&lang=${lang}`
+  )
+  const data = await response.json()
+
+  const uris = data.map((node) => {
+    return {
+      params: {
+        uri: node.slug
       }
-    })
+    }
   })
-  const { data } = await response.json()
 
-  const uris = Object.values(data)
-    .reduce(function (acc, currentValue) {
-      return acc.concat(currentValue.nodes)
-    }, [])
-    .filter((node) => node.uri !== null)
-    .map((node) => {
-      let trimmedURI = node.uri.substring(1)
-      trimmedURI = trimmedURI.substring(0, trimmedURI.length - 1)
-
-      // Remove `en/ and fr/ from the URI to allow the right path at build time
-      // en/ Page are already within a /en folder`
-      trimmedURI = trimmedURI.replace('en/', '')
-      trimmedURI = trimmedURI.replace('fr/', '')
-      return {
-        params: {
-          uri: trimmedURI
-        }
-      }
-    })
+  console.log(lang + ` uris posts`)
+  console.log(uris)
 
   return uris
 }
