b8f0932e260cec6fd4f97a841e9ad390b35923f4
Sebastien Delrue <seb.delrue@gmail.com>
2023-11-08 20:39:28 -0500

    Oups, wrong env name

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,6 +2,6 @@ export default function parseHtml(html: string) {
   // Replace all internal links with Astro app url
   return html.replace(
     `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
-    `href="${import.meta.env.WORDPRESS_ASTRO_APP_VERCEL_URL}`
+    `href="${import.meta.env.WORDPRESS_ASTRO_APP_URL}`
   )
 }
