7224ef2273d711a7314abba58ccb1818412590e7
Sebastien Delrue <seb.delrue@gmail.com>
2023-11-09 18:27:17 -0500

    Use replaceAll within parser...

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,6 +1,6 @@
 export default function parseHtml(html: string) {
   // Replace all internal links with Astro app url
-  return html.replace(
+  return html.replaceAll(
     `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
     `href="https://${import.meta.env.VERCEL_URL}`
   )
