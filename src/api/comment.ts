0aefb961af77c739b2704130204b1137f29481f4
Sebastien Delrue <seb.delrue@gmail.com>
2023-11-17 15:15:38 -0500

    Add post comments

---
+++ src/api/comment.ts
@@ -0,0 +1,52 @@
+const FETCH_PER_PAGE = 100 // Lower this number if it causes connection issues to your hosting. It cannot be greater than 100.
+
+export async function getCommentsForPost(postId: number, lang: string = 'fr') {
+  let page = 1
+
+  let comments = await getRecursiveCommentsForPost(postId, lang, page)
+
+  while (comments.length % FETCH_PER_PAGE === 0) {
+    page = page + 1
+    const newComments = await getRecursiveCommentsForPost(postId, lang, page)
+    comments = comments.concat(newComments)
+  }
+
+  return comments
+}
+
+const getRecursiveCommentsForPost = async (
+  postId: number,
+  lang: string,
+  page: number
+): Promise<Comments[]> => {
+  const res = await fetch(
+    import.meta.env.WORDPRESS_REST_API_URL +
+      `/comments/?per_page=${FETCH_PER_PAGE}&post=${postId}&lang=${lang}${
+        page > 1 ? '&page=' + page : ''
+      }`
+  )
+  const comments: Comments[] = await res.json()
+
+  return comments
+}
+
+export type Comments = {
+  id: number
+  post: number
+  parent: number
+  author: number
+  author_name: string
+  author_url: string
+  date: string
+  date_gmt: string
+  content: {
+    rendered: { __html: string | TrustedHTML }
+  }
+  status: string
+  modified: string
+  modified_gmt: string
+  type: 'comment'
+  meta: {
+    product_rating: number
+  }
+}
