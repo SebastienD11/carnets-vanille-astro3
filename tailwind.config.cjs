b1120fc991797c61c0776cadf484750757996ea9
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-08 18:02:43 -0500

    Try Radix Dialog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -139,6 +139,20 @@ module.exports = {
       },
       borderRadius: {
         large: '50px'
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
+        },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' }
+        }
+      },
+      animation: {
+        overlayShow: 'overlayShow 1050ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 1050ms cubic-bezier(0.16, 1, 0.3, 1)'
       }
     }
   },
