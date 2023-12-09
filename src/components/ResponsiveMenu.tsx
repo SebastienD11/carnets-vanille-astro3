5522347814e2bd87f0673eb7718f2fcdc0f35de7
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-08 19:54:31 -0500

    Basic fade out animation

--- src/components/ResponsiveMenu.tsx
+++ src/components/ResponsiveMenu.tsx
@@ -28,8 +28,8 @@ const Drawer = ({ children, open, onOpenChange }: PropsWithChildren<DrawerProps>
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black data-[state=open]:animate-overlayShow" />
-        <Dialog.Content className="fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
+        <Dialog.Overlay className="data-[state=closed]:animate-overlayHide fixed inset-0 bg-black data-[state=open]:animate-overlayShow" />
+        <Dialog.Content className="data-[state=closed]:animate-contentHide fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
           {children}
           <Dialog.Close asChild>
             <button
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -145,14 +145,24 @@ module.exports = {
           from: { opacity: 0 },
           to: { opacity: 1 }
         },
+        overlayHide: {
+          from: { opacity: 1 },
+          to: { opacity: 0 }
+        },
         contentShow: {
-          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.90)' },
           to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' }
+        },
+        contentHide: {
+          from: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+          to: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.90)' }
         }
       },
       animation: {
         overlayShow: 'overlayShow 1050ms cubic-bezier(0.16, 1, 0.3, 1)',
-        contentShow: 'contentShow 1050ms cubic-bezier(0.16, 1, 0.3, 1)'
+        contentShow: 'contentShow 1050ms cubic-bezier(0.16, 1, 0.3, 1)',
+        overlayHide: 'overlayHide 1050ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentHide: 'contentHide 1050ms cubic-bezier(0.16, 1, 0.3, 1)'
       }
     }
   },
