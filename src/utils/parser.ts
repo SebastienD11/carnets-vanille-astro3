abde53959951fb732393e49ee03b28a3f88c039d
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-11 15:56:13 -0500

    Parse Link for LinkCard component

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,7 +1,15 @@
-export default function parseHtml(html: string) {
+export function parseHtml(html: string) {
   // Replace all internal links with Astro app url
   return html.replaceAll(
     `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
     `href="https://${import.meta.env.VERCEL_URL}`
+  )
+}
+
+export function replaceEnvUrls(input: string) {
+  // Replace all internal links with Astro app url
+  return input.replaceAll(
+    import.meta.env.WORDPRESS_INDEX_URL,
+    `https://${import.meta.env.VERCEL_URL}`
   )
 }
