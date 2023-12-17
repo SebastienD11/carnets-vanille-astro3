88d67f979a63047c477f58ee203b34546683c00f
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-17 14:12:59 -0500

    Basic fetch for testing

--- src/pages/api/mailerlite.ts
+++ src/pages/api/mailerlite.ts
@@ -1,53 +1,26 @@
 import type { APIRoute } from 'astro'
+import MailerLite, { type GroupObject } from '@mailerlite/mailerlite-nodejs'
 
-export const GET: APIRoute = () => {
-  // const mailerLiteApiKey = process.env.MAILERLITE_KEY
+export const GET: APIRoute = async () => {
+  const mailerLiteApiKey = import.meta.env.MAILERLITE_KEY
+  const mailerlite = new MailerLite({
+    api_key: mailerLiteApiKey
+  })
+  let groups = []
 
-  return new Response(
-    JSON.stringify({
-      mode: import.meta.env.MODE,
-      env: import.meta.env.SSR ? 'ssr' : 'not ssr',
-      mailerlite: import.meta.env.MAILERLITE_KEY
-        ? import.meta.env.MAILERLITE_KEY
-        : 'nothing with meta.env',
-      mailerliteProcess: process.env.MAILERLITE_KEY
-        ? process.env.MAILERLITE_KEY
-        : 'nothing with process'
+  const params = {
+    limit: 25,
+    page: 1
+  }
+
+  await mailerlite.groups
+    .get(params)
+    .then((response) => {
+      groups = response.data
     })
-  )
+    .catch((error) => {
+      if (error.response) console.log(error.response.data)
+    })
+
+  return new Response(JSON.stringify(groups))
 }
-
-// import MailerLite from '@mailerlite/mailerlite-nodejs'
-// const mailerlite = new MailerLite({
-//   api_key: mailerLiteApiKey
-// })
-
-// console.log(mailerLiteApiKey)
-// const params = {
-//   limit: 25,
-//   page: 1,
-//   filter: {
-//     name: 'dummy'
-//   },
-//   sort: '-name'
-// }
-
-// mailerlite.groups
-//   .get(params)
-//   .then((response) => {
-//     console.log(response.data)
-//   })
-//   .catch((error) => {
-//     if (error.response) console.log(error.response.data)
-//   })
-// }
-
-// export const GET: APIRoute = () => {
-//   const myKey = import.meta.env.MY_KEY
-
-//   return new Response(
-//     JSON.stringify({
-//       key: myKey
-//     })
-//   )
-// }
