d9f1c0c7e6d5b8ff1354fc651704f514ca866006
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-18 18:21:50 -0500

    NEwsletter form content

--- src/api/mailerlite.ts
+++ src/api/mailerlite.ts
@@ -1,9 +1,7 @@
-// SSR API used client side at runtime.
-
-import type { APIRoute } from 'astro'
+// SSR API Used at build time.
 import MailerLite, { type GroupObject } from '@mailerlite/mailerlite-nodejs'
 
-export const GET: APIRoute = async () => {
+export const getMailerliteGroups = async () => {
   const mailerLiteApiKey = import.meta.env.MAILERLITE_KEY
   const mailerlite = new MailerLite({
     api_key: mailerLiteApiKey
@@ -11,7 +9,9 @@ export const GET: APIRoute = async () => {
   let groups: GroupObject[] = []
 
   await mailerlite.groups
-    .get({ sort: 'name' })
+    .get({
+      sort: 'name'
+    })
     .then((response) => {
       groups = response.data.data
     })
@@ -19,5 +19,5 @@ export const GET: APIRoute = async () => {
       if (error.response) console.log(error.response.data)
     })
 
-  return new Response(JSON.stringify(groups))
+  return groups
 }
