cd23743a11fdd38233f9974370b981b460aaf04b
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-27 14:59:49 -0500

    Start Free Ebook Banner

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,9 +1,24 @@
+import { parse } from 'node-html-parser'
+
 export function parseHtml(html: string) {
-  // Replace all internal links with Astro app url
+  html = replaceInternalLinks(html)
+  html = replaceFreeBookShortcode(html)
+
+  return html
+}
+
+function replaceInternalLinks(html: string) {
   return html.replaceAll(
     `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
     `href="https://${import.meta.env.VERCEL_URL}`
   )
+}
+
+function replaceFreeBookShortcode(html: string) {
+  const parsedHtml = parse(html)
+  parsedHtml.querySelector('[shortcode_free_ebook_banner]')?.replaceWith('FREE EBOOK BANNER')
+
+  return parsedHtml.toString()
 }
 
 export function replaceEnvUrls(input: string) {
