e21cf3fc194021498801a5dccfd4069ad0fecc4f
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-27 15:46:56 -0500

    Replace WP element by Astro component

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,30 +1,14 @@
-import { parse } from 'node-html-parser'
-
 export function parseHtml(html: string) {
   html = replaceInternalLinks(html)
-  html = replaceFreeBookShortcode(html)
 
   return html
 }
 
 function replaceInternalLinks(html: string) {
-  return html.replaceAll(
-    `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
-    `href="https://${import.meta.env.VERCEL_URL}`
-  )
-}
-
-function replaceFreeBookShortcode(html: string) {
-  const parsedHtml = parse(html)
-  parsedHtml.querySelector('[shortcode_free_ebook_banner]')?.replaceWith('FREE EBOOK BANNER')
-
-  return parsedHtml.toString()
+  return html.replaceAll(`data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`, `href="https://${import.meta.env.VERCEL_URL}`)
 }
 
 export function replaceEnvUrls(input: string) {
   // Replace all internal links with Astro app url
-  return input.replaceAll(
-    import.meta.env.WORDPRESS_INDEX_URL,
-    `https://${import.meta.env.VERCEL_URL}`
-  )
+  return input.replaceAll(import.meta.env.WORDPRESS_INDEX_URL, `https://${import.meta.env.VERCEL_URL}`)
 }
