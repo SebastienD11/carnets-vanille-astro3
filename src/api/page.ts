dfdfe5cd67895599542cf6a232247bff1a1b3593
Sebastien Delrue <seb.delrue@gmail.com>
2023-12-28 10:22:59 -0500

    Landing Page template

--- src/api/page.ts
+++ src/api/page.ts
@@ -1,10 +1,58 @@
 export async function getPageBySlug(slug: string, lang: string): Promise<Page | null> {
   const res = await fetch(
-    import.meta.env.WORDPRESS_REST_API_URL + `/pages/?slug=${slug}&lang=${lang}`
+    import.meta.env.WORDPRESS_REST_API_URL + `/pages/?slug=${slug}&lang=${lang}&acf_format=standard`
   )
   const page: Page[] = await res.json()
 
   return page.length > 0 ? page[0] : null
+}
+
+type PageSection = {
+  section_title: string
+  section_icon: string
+  section_intro: string
+  section_banner: boolean
+  section_banner_image: {
+    ID: number
+    id: number
+    title: string
+    filename: string
+    filesize: number
+    url: string
+    link: string
+    alt: string
+    author: string
+    description: string
+    caption: string
+    name: string
+    status: string
+    uploaded_to: number
+    date: string
+    modified: string
+    menu_order: number
+    mime_type: string
+    type: string
+    subtype: string
+    icon: string
+    width: number
+    height: number
+    sizes: any[]
+  }
+  section_banner_text: string
+  section_banner_link: {
+    title: string
+    url: string
+    target: string
+  }
+  section_articles: {
+    ID: number
+  }[]
+  section_read_more_label: string
+  section_read_more_link: {
+    title: string
+    url: string
+    target: string
+  }
 }
 
 export type Page = {
@@ -33,7 +81,13 @@ export type Page = {
   comment_status: string
   sticky: boolean
   template: string
-  acf: []
+  acf: {
+    introduction_title: string
+    introduction_intro: string
+    banner_ebook: boolean
+    sections: PageSection[]
+    template: string
+  }
   yoast_head: string
   wpml_current_locale: string
   wpml_translations: {
--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -5,10 +5,23 @@ export function parseHtml(html: string) {
 }
 
 function replaceInternalLinks(html: string) {
-  return html.replaceAll(`data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`, `href="https://${import.meta.env.VERCEL_URL}`)
+  return html.replaceAll(
+    `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
+    `href="https://${import.meta.env.VERCEL_URL}`
+  )
+}
+
+export function replaceLinks(html: string) {
+  return html.replaceAll(
+    import.meta.env.WORDPRESS_INDEX_URL,
+    'https://' + import.meta.env.VERCEL_URL
+  )
 }
 
 export function replaceEnvUrls(input: string) {
   // Replace all internal links with Astro app url
-  return input.replaceAll(import.meta.env.WORDPRESS_INDEX_URL, `https://${import.meta.env.VERCEL_URL}`)
+  return input.replaceAll(
+    import.meta.env.WORDPRESS_INDEX_URL,
+    `https://${import.meta.env.VERCEL_URL}`
+  )
 }
