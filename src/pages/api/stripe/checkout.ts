8069f35f4040afa72816ff21087bdeb48d67a687
Sebastien Delrue <seb.delrue@gmail.com>
2024-01-22 18:42:23 -0500

    Pass variables from client

--- src/pages/api/stripe/checkout.ts
+++ src/pages/api/stripe/checkout.ts
@@ -3,20 +3,21 @@
 import type { APIRoute } from 'astro'
 import Stripe from 'stripe'
 
-export const POST: APIRoute = async ({ request, redirect }) => {
+export const POST: APIRoute = async ({ request }) => {
   if (request.headers.get('Content-Type') === 'application/json') {
     const stripe = new Stripe(import.meta.env.ASTRO_APP_STRIPE_SECRET_KEY)
-
+    const body = await request.json()
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
-          price: 'price_1NKStCKYHvAdiOgznb8mBdM9',
+          price: body.stripePriceId,
           quantity: 1
         }
       ],
+      allow_promotion_codes: true,
       mode: 'payment',
-      success_url: `https://localhost:4321/ebook-saint-malo?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://localhost:4321/ebook-saint-malo`
+      success_url: `${body.page.href}?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: body.page.href
     })
 
     return new Response(JSON.stringify({ url: session.url }), {
