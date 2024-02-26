09caa6742d81b25dbc40e82a5156092d3ed30b11
Sebastien Delrue <seb.delrue@gmail.com>
2024-02-26 18:46:24 -0500

    Update parser utils

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -5,23 +5,25 @@ export function parseHtml(html: string) {
 }
 
 function replaceInternalLinks(html: string) {
-  return html.replaceAll(
-    `data-internal-link="true" href="${import.meta.env.WORDPRESS_INDEX_URL}`,
+  const firstReplace = html.replaceAll(
+    `data-internal-link="true" href="${import.meta.env.BLOG_URL}`,
+    `href="https://${import.meta.env.VERCEL_URL}`
+  )
+
+  return firstReplace.replaceAll(
+    `data-internal-link="true" href="${import.meta.env.WORDPRESS_CMS_URL}`,
     `href="https://${import.meta.env.VERCEL_URL}`
   )
 }
 
-export function replaceLinks(html: string) {
-  return html.replaceAll(
-    import.meta.env.WORDPRESS_INDEX_URL,
+export function replaceUrls(html: string) {
+  const firstReplace = html.replaceAll(
+    import.meta.env.BLOG_URL,
     'https://' + import.meta.env.VERCEL_URL
   )
-}
 
-export function replaceEnvUrls(input: string) {
-  // Replace all internal links with Astro app url
-  return input.replaceAll(
-    import.meta.env.WORDPRESS_INDEX_URL,
-    `https://${import.meta.env.VERCEL_URL}`
+  return firstReplace.replaceAll(
+    import.meta.env.WORDPRESS_CMS_URL,
+    'https://' + import.meta.env.VERCEL_URL
   )
 }
