edff98f39e9d294716621d35fb9782a4526a104c
Sebastien Delrue <seb.delrue@gmail.com>
2024-02-27 18:21:03 -0500

    Update internal links and URLs in parser.ts

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -7,23 +7,17 @@ export function parseHtml(html: string) {
 function replaceInternalLinks(html: string) {
   const firstReplace = html.replaceAll(
     `data-internal-link="true" href="${import.meta.env.BLOG_URL}`,
-    `href="https://${import.meta.env.VERCEL_URL}`
+    `href="${import.meta.env.BUILD_URL}`
   )
 
   return firstReplace.replaceAll(
     `data-internal-link="true" href="${import.meta.env.WORDPRESS_CMS_URL}`,
-    `href="https://${import.meta.env.VERCEL_URL}`
+    `href="${import.meta.env.BUILD_URL}`
   )
 }
 
 export function replaceUrls(html: string) {
-  const firstReplace = html.replaceAll(
-    import.meta.env.BLOG_URL,
-    'https://' + import.meta.env.VERCEL_URL
-  )
+  const firstReplace = html.replaceAll(import.meta.env.BLOG_URL, import.meta.env.BUILD_URL)
 
-  return firstReplace.replaceAll(
-    import.meta.env.WORDPRESS_CMS_URL,
-    'https://' + import.meta.env.VERCEL_URL
-  )
+  return firstReplace.replaceAll(import.meta.env.WORDPRESS_CMS_URL, import.meta.env.BUILD_URL)
 }
