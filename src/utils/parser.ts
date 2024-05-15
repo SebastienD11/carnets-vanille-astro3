ad99b1dacde35712c71bbf44de9109fd557788a8
Sebastien Delrue <seb.delrue@gmail.com>
2024-05-15 08:46:21 -0400

    Improve URL parser

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,3 +1,7 @@
+const url = import.meta.env.VERCEL_URL
+  ? 'https://' + import.meta.env.VERCEL_URL
+  : import.meta.env.BUILD_URL
+
 export function parseHtml(html: string) {
   html = replaceInternalLinks(html)
 
@@ -7,17 +11,17 @@ export function parseHtml(html: string) {
 function replaceInternalLinks(html: string) {
   const firstReplace = html.replaceAll(
     `data-internal-link="true" href="${import.meta.env.BLOG_URL}`,
-    `href="${import.meta.env.BUILD_URL}`
+    `href="${url}`
   )
 
   return firstReplace.replaceAll(
     `data-internal-link="true" href="${import.meta.env.WORDPRESS_CMS_URL}`,
-    `href="${import.meta.env.BUILD_URL}`
+    `href="${url}`
   )
 }
 
 export function replaceUrls(html: string) {
-  const firstReplace = html.replaceAll(import.meta.env.BLOG_URL, import.meta.env.BUILD_URL)
+  const firstReplace = html.replaceAll(import.meta.env.BLOG_URL, url)
 
-  return firstReplace.replaceAll(import.meta.env.WORDPRESS_CMS_URL, import.meta.env.BUILD_URL)
+  return firstReplace.replaceAll(import.meta.env.WORDPRESS_CMS_URL, url)
 }
