48bdbe3caf8fa41ab2ed59d37dd2c35740f2d979
Sebastien Delrue <seb.delrue@gmail.com>
2024-05-15 10:33:19 -0400

    Fix parser

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,6 +1,9 @@
-const url = import.meta.env.VERCEL_URL
-  ? 'https://' + import.meta.env.VERCEL_URL
-  : import.meta.env.BUILD_URL
+const url =
+  import.meta.env.VERCEL_ENV === 'production'
+    ? import.meta.env.BUILD_URL
+    : import.meta.env.VERCEL_URL
+      ? 'https://' + import.meta.env.VERCEL_URL
+      : import.meta.env.BUILD_URL
 
 export function parseHtml(html: string) {
   html = replaceInternalLinks(html)
