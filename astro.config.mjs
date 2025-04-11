d993167b190ad1a1dcc0168f26894c8f459a7c30
Sebastien Delrue <seb.delrue@gmail.com>
2025-04-11 16:09:01 -0400

    Bump to Tailwind V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import lifecycleLogs from './src/integrations/lifecycle-logs'
-import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 import redirectList from '/redirectsList.json'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), lifecycleLogs()],
+  integrations: [lifecycleLogs()],
   i18n: {
     defaultLocale: 'fr',
     locales: ['en', 'fr'],
@@ -21,7 +21,7 @@ export default defineConfig({
     domains: ['content.carnetsvanille.com']
   },
   vite: {
-    plugins: [basicSsl()]
+    plugins: [basicSsl(), tailwindcss()]
   },
   redirects: redirectList
 })
