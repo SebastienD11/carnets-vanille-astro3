428c13ed55dea1490b657b8023df6b38fab49420
Sebastien Delrue <seb.delrue@gmail.com>
2025-11-25 09:41:23 +0100

    Update Plausible Script

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  plausible?: {
+    (eventName: string, options?: { props?: Record<string, string | number | boolean> }): void
+    q: any[]
+    init: (options?: Record<string, any>) => void
+    o?: Record<string, any>
+  }
+}
