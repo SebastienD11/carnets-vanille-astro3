c5827d0e9cdf3bc401f80e97e2bd400d3e6c9162
Sebastien Delrue <seb.delrue@gmail.com>
2025-11-25 10:00:08 +0100

    Revert "Update Plausible Script"
    This reverts commit 428c13ed55dea1490b657b8023df6b38fab49420.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface Window {
-  plausible?: {
-    (eventName: string, options?: { props?: Record<string, string | number | boolean> }): void
-    q: any[]
-    init: (options?: Record<string, any>) => void
-    o?: Record<string, any>
-  }
-}
